bd91453c37c4bbd2f87088c777ca913a3fe3af0d
Danton Ruthe <37008083+DantonR@users.noreply.github.com>
2021-11-22 16:45:11 +1300

    Added image capability

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 var editor = new EditorJS({
   // Other configuration properties
   tools: {
+    image: {
+      class: ImageTool,
+      config: {
+        endpoints: {
+          byFile: "http://localhost:8008/uploadFile", // Your backend file uploader endpoint
+          byUrl: "http://localhost:8008/fetchUrl", // Your endpoint that provides uploading by Url
+        },
+      },
+    },
+
     header: {
       class: Header,
       config: {
